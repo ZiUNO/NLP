8eefd5319582205d6a0dc871e232b0453b0287f2
ZiUNO <331245059@qq.com>
2019-07-03 23:32:46 +0800

    Add: 语义还原和删除停用词

--- nlp.py
+++ nlp.py
@@ -5,14 +5,21 @@
 * @Time: 2019/7/3 10:26
 """
 
-from sklearn.feature_extraction.text import CountVectorizer
-from scipy.stats import pearsonr
 import os
 import string
-import zhon.hanzi as hanzi
-import numpy as np
+
 import matplotlib.pyplot as plt
+import numpy as np
 import scipy
+import zhon.hanzi as hanzi
+from nltk import pos_tag
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from scipy.stats import pearsonr
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+stop_word = list(set(stopwords.words('english')))
 
 
 def get_data(path):
@@ -33,16 +40,35 @@ def get_data(path):
     return g_content, i_content
 
 
-def remove_useless_info(s):
+def __lemmatizer(s):
+    s = s.split(' ')
+    wordnet_lemmatizer = WordNetLemmatizer()
+    s = [wordnet_lemmatizer.lemmatize(i, pos='v') for i in s]
+    s = ' '.join(s)
+    return s
+
+
+def __remove_stop_word(s):
+    s = word_tokenize(s)
+    pos = pos_tag(s)
+    s = [item for i, item in enumerate(s) if not item in stop_word and pos[i][1] != 'POS'] # FIXME [__remove_stop_word] 当POS为NNP的时候如果长度为1的情况需要剔除
+    # TODO 当POS为NNP的时候，需要注意名词为表示人物的名词，寻找将人物名词转换为词根方法
+    print(pos_tag(s))
+    return ' '.join(s)
+
+def participle(s):
     """
-    去除中英文标点及停用词
+    处理原始字符串，进行分词操作
     :param s: 原始字符串
     :return: 处理后的的字符串
     """
     en_trantab = str.maketrans({key: None for key in string.punctuation})
     ch_trantab = str.maketrans({key: None for key in hanzi.punctuation})
+    s = s.lower()
+    s = __remove_stop_word(s)
     s = s.translate(en_trantab)
     s = s.translate(ch_trantab)
+    s = __lemmatizer(s)
     return s
 
 
@@ -52,9 +78,9 @@ def vectorization(v):
     :param v: 文本列表
     :return: 向量化后的列表
     """
-    tfidf = CountVectorizer()  # 可以更改为其他的向量化方法
-    re = tfidf.fit_transform(v).toarray()
-    return re
+    tfidf = TfidfVectorizer()  # 可以更改为其他的向量化方法
+    rho = tfidf.fit_transform(v).toarray()
+    return rho
 
 
 def similarity(v1, v2):
@@ -90,13 +116,18 @@ output = []
 for index, ic in enumerate(input_content):
     ic0 = ic[0]
     ic1 = ic[1]
-    ic0_p = remove_useless_info(ic0)
-    ic1_p = remove_useless_info(ic1)
+    ic0_p = participle(ic0)
+    ic1_p = participle(ic1)
     v0_p, v1_p = vectorization([ic0_p, ic1_p])
     sim = similarity(v0_p, v1_p)
     output.append(sim)
-    print("similarity: %f, output: %f" % (sim, golden_content[index]))
+    print("i: %d, similarity: %f, output: %f, s1: %s, s2: %s" % (index, sim, golden_content[index], ic0_p, ic1_p))
 r, p = pearson(output, golden_content)
 print(r)
 plt.scatter(golden_content, output, s=10)
 plt.show()
+s = []
+for i, item in enumerate(output):
+    s.append('%f\t%s\t%s' % (item, input_content[i][0], input_content[i][1]))
+with open('output.txt', 'w', encoding='utf-8') as f:
+    f.write('\n'.join(s))
